1dbbc3642943e534e38b54a4f098b11a6a3aec37
zinczar <brendan@wineisneat.com>
2019-07-28 20:04:41 +0000

    added functioning code

---
+++ _printf.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "holberton.h"
+
+int _printf(char *format, ...)
+{
+        va_list arguments;
+        int i;
+        int (*fptr)(va_list arguments);
+
+        va_start(arguments, format);
+
+        for (i = 0; format[i] != '\0'; i++)
+        {
+                if (format[i] == '%')
+                {
+                        i++;
+                        fptr = get_percent(format[i]);
+                        if (fptr == NULL)
+                                return (0);
+                        fptr(arguments);
+                }
+                else
+                        _putchar(format[i]);
+        }
+        va_end(arguments);
+        return (0);
+}
