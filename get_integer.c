5720fa394200c528ce95e7c0373932646240ffc1
guilmeister <ebg.edwardbguillermo@yahoo.com>
2019-07-29 02:52:38 +0000

    added functioning integer flag

--- get_integer.c
+++ get_integer.c
@@ -2,8 +2,32 @@
 #include <stdio.h>
 #include <stdarg.h>
 
+int print_integer(int number)
+{
+	if(number < 0)
+	{
+		_putchar('-');
+		number = -number;
+	}
+
+	if (number == 0)
+		return (0);
+
+	if (number / 10 != 0)
+		print_integer(number / 10);
+
+	_putchar(number % 10 + '0');
+
+	return (number);
+}
+
 int get_integer(va_list arguments)
 {
-        (void)arguments;
+	int integer;
+
+	integer = va_arg(arguments, int);
+
+	print_integer(integer);
+
         return (0);
 }
