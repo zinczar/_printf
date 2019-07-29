cbe963ecf0d9bcd5327200db682ac8c1d281f902
guilmeister <ebg.edwardbguillermo@yahoo.com>
2019-07-29 17:53:30 +0000

    added file to master

--- _printf.c
+++ _printf.c
@@ -2,30 +2,35 @@
 #include <stdarg.h>
 #include "holberton.h"
 
+/**
+ * _printf - prints all values
+ *
+ * @format: value from main
+ *
+ * Return: Always 0 (Success)
+ */
+
 int _printf(char *format, ...)
 {
-        va_list arguments;
-	int count = 0;
-        int i;
-        int (*fptr)(va_list arguments);
+	va_list arguments;
+	int i;
+	int (*fptr)(va_list arguments);
 
-        va_start(arguments, format);
-	if (format)
+	va_start(arguments, format);
+
+	for (i = 0; format[i] != '\0'; i++)
 	{
-		for (i = 0; format[i] != '\0'; i++)
+		if (format[i] == '%')
 		{
-			if (format[i] == '%')
-			{
-				i += 2;
-				fptr = get_percent(format[i]);
-				if (fptr == NULL)
-					return (0);
-				count += fptr(arguments);
-			}
-			else
-				count +=_putchar(format[i]);
+			i++;
+			fptr = get_percent(format[i]);
+			if (fptr == NULL)
+				return (0);
+			fptr(arguments);
 		}
+		else
+			_putchar(format[i]);
 	}
 	va_end(arguments);
-        return (count);
+	return (0);
 }
