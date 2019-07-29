917aac0bb579bb5041a315f9e9680a0cbb48a7a6
guilmeister <ebg.edwardbguillermo@yahoo.com>
2019-07-29 02:52:25 +0000

    added functioning decimal flag

--- get_decimal.c
+++ get_decimal.c
@@ -2,8 +2,33 @@
 #include <stdio.h>
 #include <stdarg.h>
 
+int print_decimal(int number)
+{
+	if(number < 0)
+	{
+		_putchar('-');
+		number = -number;
+	}
+
+	if (number == 0)
+		return (0);
+
+	if (number / 10 != 0)
+		print_decimal(number / 10);
+
+	_putchar(number % 10 + '0');
+
+	return (number);
+}
+
+
 int get_decimal(va_list arguments)
 {
-        (void)arguments;
-        return (0);
+	int decimal;
+
+	decimal = va_arg(arguments, int);
+
+	print_decimal(decimal);
+
+	return (0);
 }
