dc28a57fc9ce116d86e34cf469309b6a611294d4
guilmeister <ebg.edwardbguillermo@yahoo.com>
2019-07-29 17:52:09 +0000

    fixed format

--- _printf.c
+++ _printf.c
@@ -2,27 +2,35 @@
 #include <stdarg.h>
 #include "holberton.h"
 
+/**
+ * _printf - prints all values
+ *
+ * @format: value from main
+ *
+ * Return: Always 0 (Success)
+ */
+
 int _printf(char *format, ...)
 {
-        va_list arguments;
-        int i;
-        int (*fptr)(va_list arguments);
+	va_list arguments;
+	int i;
+	int (*fptr)(va_list arguments);
 
-        va_start(arguments, format);
+	va_start(arguments, format);
 
-        for (i = 0; format[i] != '\0'; i++)
-        {
-                if (format[i] == '%')
-                {
-                        i++;
-                        fptr = get_percent(format[i]);
-                        if (fptr == NULL)
-                                return (0);
-                        fptr(arguments);
-                }
-                else
-                        _putchar(format[i]);
-        }
-        va_end(arguments);
-        return (0);
+	for (i = 0; format[i] != '\0'; i++)
+	{
+		if (format[i] == '%')
+		{
+			i++;
+			fptr = get_percent(format[i]);
+			if (fptr == NULL)
+				return (0);
+			fptr(arguments);
+		}
+		else
+			_putchar(format[i]);
+	}
+	va_end(arguments);
+	return (0);
 }
